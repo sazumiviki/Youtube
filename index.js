2e99a34feebda6f18b13b32338b01b082833a59e
Sazumi Viki <146317698+sazumiviki@users.noreply.github.com>
2023-11-16 23:40:32 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cheerio from 'cheerio';
 const fetch = import('node-fetch');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const post = async (url, form, headers = {}) => {
     const fetchModule = await fetch;
